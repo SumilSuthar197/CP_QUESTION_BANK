05c731befabb309505dddf00b933873b40f420aa
Sarthak0714 <91636065+sarthak0714@users.noreply.github.com>
2022-07-09 01:15:23 +0530

    Update 32.c

--- 32.c
+++ 32.c
@@ -1,3 +1,23 @@
+//PS THE QUESTION IS NOT CLEAR AND THERE ARE 2 POSSIBLITIES OF ANSWERS
+
+//IF SUM OF N NATURAL NOS USING RECURSIVE
+#include<stdio.h>
+int sm(int n){
+    if(n==1)
+        return 1;
+    else
+        return sm(n-1)+n;
+}
+void main(){
+    int n,s;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    s=sm(n);
+    printf("The sum of %d numbers is %d.",n,s);
+}
+
+// IF SUM OF N GIVEN NUMBERS USING RECURSIVE
+
 #include<stdio.h>
 int sumn(int a[],int n){
     int sum=0;
