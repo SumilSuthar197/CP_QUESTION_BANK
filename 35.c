639ababdca9c3b450534dfbe0cd4299ee3773e85
Sumil Suthar <102129056+SumilSuthar197@users.noreply.github.com>
2022-07-09 23:00:37 +0530

    Update 35.c

--- 35.c
+++ 35.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-    int i,j,r,c,d1=0,d2=0,common=0;
+    int i,j,r,c,common=0;
     int a[100][100];
     printf("enter the no of rows and column:");
     scanf("%d%d",&r,&c);
@@ -20,20 +20,12 @@ void main()
     {
         for(j=0;j<c;j++)
         {
-            if(i==j)
-            {
-                d1=d1+a[i][j];
-            }
-            if((i+j)==(r-1))
-            {
-                d2=d2+a[i][j];
-            }
-            if((i==j)&&((i+j)==(r-1)))
+            if((i==j)||((i+j)==(r-1)))
             {
                 common=common+a[i][j];
             }
         }
     }
     
-    printf("the sum of diagonal element of both sides is %d",d1+d2-common);
+    printf("the sum of diagonal element of both sides is %d",common);
 }
