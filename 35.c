67a09278d5f03a292aef989bde2c01f8fa54b14f
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-07-10 13:18:26 +0530

    remove c variable
    square matrix

--- 35.c
+++ 35.c
@@ -1,31 +1,30 @@
 #include<stdio.h>
 void main()
 {
-    int i,j,r,c,common=0;
+    int i,j,r,sum=0;
     int a[100][100];
-    printf("enter the no of rows and column:");
-    scanf("%d%d",&r,&c);
+    printf("enter the no of rows");
+    scanf("%d",&r);
 
     printf("\nenter the matrix A");
     for(i=0;i<r;i++)
     {
-        for(j=0;j<c;j++)
+        for(j=0;j<r;j++)
         {
             printf("\nenter the element:");
             scanf("%d",&a[i][j]);
         }
     }
-
     for(i=0;i<r;i++)
     {
-        for(j=0;j<c;j++)
+        for(j=0;j<r;j++)
         {
             if((i==j)||((i+j)==(r-1)))
             {
-                common=common+a[i][j];
+                sum=sum+a[i][j];
             }
         }
     }
-    
-    printf("the sum of diagonal element of both sides is %d",common);
+
+    printf("the sum of diagonal element of both sides is %d",sum);
 }
