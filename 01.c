12f7d38bc2a5bc4cb3229805fd25b6828f1dd415
Sumil Suthar <102129056+SumilSuthar197@users.noreply.github.com>
2022-07-10 17:25:08 +0530

    Update 01.c

--- 01.c
+++ 01.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
+struct book
+{
+  char title[30];
+  char author[30]; 
+  int price;
+};
 void main()
 {
     int i,j, n = 5;
-    struct book
-    {
-        char title[30];
-        char author[30];
-        int price;
-    };
+    
     struct book a[n],temp;
     for (i=0;i<n;i++)
     {
         printf("Enter Title, Author and Price of Book %d\n", i+1);
-        scanf("%s %s %d", &a[i].title,&a[i].author,&a[i].price);
+        scanf("%s %s %d", a[i].title,a[i].author,&a[i].price);
     }
     for (i=0;i<n;i++)
     {
