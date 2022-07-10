840fa150fed4b8420590d08bb8e47da359ff5824
Sumil Suthar <102129056+SumilSuthar197@users.noreply.github.com>
2022-07-10 21:12:50 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 void main()
 {
-    int a=0,b=1,c=0,n,i=0;
+    int a=0,b=1,c,n,i;
     printf("enter the number of term in fibonacci series:");
     scanf("%d",&n);
-    printf("\n fibonacci series:\n");
+    printf("\nfibonacci series:\n");
     printf("%d\t%d",a,b);
 
-    while(i<n)
+
+    for(i=0;i<n-2;i++)
     {
+        c=a+b;
         printf("\t%d",c);
         a=b;
         b=c;
-        c=a+b;
-        i++;
     }
-}
